e65f51f6b072dbdc5fd98eb61bd62741375d79dd
sm957 <sm957@duke.edu>
2022-09-06 20:49:29 -0400

    did13

--- 013_complex_ty/mytypes.c
+++ 013_complex_ty/mytypes.c
@@ -2,7 +2,7 @@ struct _complex_num_t {
   double real_part;
   double imaginary_part;
 };
-typedef struct _complex_num_t _complex_num_t;
+typedef struct _complex_num_t complex_num_t;
 
 enum _animal_t {
   CAT,
