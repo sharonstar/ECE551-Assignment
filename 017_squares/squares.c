5a75877fd64df6d6f8c9559dedcaa961f3237c21
sm957 <sm957@duke.edu>
2022-09-08 23:36:54 -0400

    did 18

--- 017_squares/squares.c
+++ 017_squares/squares.c
@@ -23,7 +23,7 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //     x is equal to either x_offset OR x_offset + size2 -1)
       if (((x >= x_offset && x < (x_offset + size2)) &&
            (y == y_offset || y == (y_offset + size2 - 1))) ||
-          ((y >= y_offset && y < (y_offset + size2)) &&
+          ((y >= y_offset && x < (y_offset + size2)) &&
            (x == x_offset || x == (x_offset + size2 - 1)))) {
         // if so, print a *
         printf("*");
