e2d15264f233dc774e252f3da065b72bac84d3b0
sm957 <sm957@duke.edu>
2022-09-14 17:21:59 -0400

    28

---
+++ 028_tests_power/test-power.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned power(unsigned x, unsigned y);
+
+void run_check(unsigned x, unsigned y, unsigned expected_ans) {
+  unsigned ans = power(x, y);
+  if (ans != expected_ans) {
+    printf("There is an error.");
+    exit(EXIT_FAILURE);
+  }
+}
+int main(void) {
+  run_check(2, 4, 16);
+  run_check(2, 0, 1);
+  run_check(3, 3, 27);
+  run_check(5, 6, 15625);
+  run_check(3, 2, 9);
+  run_check(1, 2, 1);
+  run_check(2, 4, 16);
+  run_check(0, 0, 1);
+  run_check(3, -2, 0);
+  return EXIT_SUCCESS;
+}
