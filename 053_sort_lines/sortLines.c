918fbe3b4768b9f4287134fd8c24c49f9051fec7
sm957 <sm957@duke.edu>
2022-10-04 23:15:35 -0400

    53

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -14,8 +14,47 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+void sortAndPrint(FILE * f) {
+  char * curr = NULL;
+  char ** lines = NULL;
+  size_t len = 0;
+  size_t i = 0;
+  while (getline(&curr, &len, f) >= 0) {
+    lines = realloc(lines, (i + 1) * sizeof(*lines));
+    lines[i] = curr;
+    curr = NULL;
+    i++;
+  }
+  free(curr);
+  sortData(lines, i);
+  for (size_t j = 0; j < i; j++) {
+    printf("%s", lines[j]);
+    free(lines[j]);
+  }
+  free(lines);
+}
+
 int main(int argc, char ** argv) {
   //WRITE YOUR CODE HERE!
-
+  if (argc == 1) {
+    if (stdin == NULL) {
+      fprintf(stderr, "stdin does not have input");
+      return EXIT_FAILURE;
+    }
+    sortAndPrint(stdin);
+  }
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      FILE * f = fopen(argv[i], "r");
+      if (f == NULL) {
+        perror("could not open");
+        return EXIT_FAILURE;
+      }
+      sortAndPrint(f);
+      if (fclose(f) != 0) {
+        return EXIT_FAILURE;
+      }
+    }
+  }
   return EXIT_SUCCESS;
 }
